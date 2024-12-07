f6fd75ca799688f0f15fe8a52774715b3505ff50
Mohamed Lamrani <mlamrani@e2r12p7.1337.ma>
2024-12-07 17:04:51 +0100

    add player mvt

--- mini-map/minimap.c
+++ mini-map/minimap.c
@@ -6,7 +6,7 @@
 /*   By: mlamrani <mlamrani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 18:17:42 by mlamrani          #+#    #+#             */
-/*   Updated: 2024/12/07 16:57:15 by mlamrani         ###   ########.fr       */
+/*   Updated: 2024/12/07 17:03:58 by mlamrani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,95 +90,18 @@ void draw_circle(t_data *img, int cx, int cy, int radius, int color)
     }
 }
 
-// void draw_player(t_data *img)
-// {
-//     int player_x;
-//     int player_y;
-//     int radius;
-//     int line_length;
-//     int line_x;
-//     int line_y;
-
-//     player_x = img->ray.posx * img->map->tile_size;
-//     player_y = img->ray.posy * img->map->tile_size;
-
-//     radius = img->map->tile_size / 2; // Smaller radius for the player circle
-//     draw_circle(img, player_x, player_y, radius, 0xFF0000); // Red circle for the player
-// }
-
-
-// Draw a line from (x0, y0) to (x1, y1)
-void draw_line(t_data *img, int x0, int y0, int x1, int y1, int color)
-{
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-    int sx = x0 < x1 ? 1 : -1;
-    int sy = y0 < y1 ? 1 : -1;
-    int err = dx - dy;
-
-    while (x0 != x1 || y0 != y1) {
-        my_put(img, x0, y0, color); // Put pixel at current position
-        int e2 = 2 * err;
-
-        if (e2 > -dy) {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dx) {
-            err += dx;
-            y0 += sy;
-        }
-    }
-}
-
-#include <math.h>
-
-void draw_line(t_data *img, int x1, int y1, int x2, int y2, int color)
-{
-    int dx = abs(x2 - x1);
-    int dy = abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
-    int err = dx - dy;
-    int e2;
-
-    while (1) {
-        my_put(img, x1, y1, color); // Plot the pixel
-
-        if (x1 == x2 && y1 == y2)
-            break;
-
-        e2 = 2 * err;
-        if (e2 > -dy) {
-            err -= dy;
-            x1 += sx;
-        }
-        if (e2 < dx) {
-            err += dx;
-            y1 += sy;
-        }
-    }
-}
-
 void draw_player(t_data *img)
 {
-    int player_x = img->ray.posx * img->map->tile_size;
-    int player_y = img->ray.posy * img->map->tile_size;
-    int radius = img->map->tile_size / 2; // Smaller radius for the player circle
-    int line_length = img->map->tile_size; // Length of the direction line
+    int player_x;
+    int player_y;
+    int radius;
 
-    // Draw the player as a circle
+    player_x = img->ray.posx * img->map->tile_size;
+    player_y = img->ray.posy * img->map->tile_size;
+
+    radius = img->map->tile_size / 2; // Smaller radius for the player circle
     draw_circle(img, player_x, player_y, radius, 0xFF0000); // Red circle for the player
-
-    // Calculate the endpoint of the line
-    int line_x = player_x + cos(img->ray.dir) * line_length;
-    int line_y = player_y + sin(img->ray.dir) * line_length;
-
-    // Draw the line representing the player's direction
-    draw_line(img, player_x, player_y, line_x, line_y, 0x00FF00); // Green line for direction
 }
-
-
 
 void draw_minimap(t_data *img)
 {
